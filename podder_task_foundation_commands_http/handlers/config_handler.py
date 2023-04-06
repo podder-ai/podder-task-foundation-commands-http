3347f5342942cde8bf7af0ba8cae796c2227f9a5
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2023-04-06 13:30:04 +0800

    Fixed response

--- podder_task_foundation_commands_http/handlers/config_handler.py
+++ podder_task_foundation_commands_http/handlers/config_handler.py
@@ -7,7 +7,7 @@ class ConfigHandler(object):
     @staticmethod
     def handle(context: Context) -> Config:
         return Config(**{
-            "name": context.config.get("plugins.commands.http.config.name"),
+            "title": context.config.get("plugins.commands.http.config.title"),
             "version": context.version,
             "description": context.config.get("plugins.commands.http.config.description")
         })
